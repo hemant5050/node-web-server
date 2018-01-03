9e5ecfdd843b8f4fd112da992f12cf4c0e6c57ef
DESKTOP-282EE12\Lenovo <hksmart5050@gmail.com>
2018-01-03 18:34:27 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+
+let app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+hbs.registerHelper('getCurrentYear', () => {
+    return (new Date()).getFullYear();
+})
+app.use((req, res, next) => {
+    let data = `${(new Date()).toString()} : ${req.method} : ${req.ips}`;
+    fs.appendFile('append.log',  data + '\n' ,(err) => {
+        console.log(err);
+    })
+    next();
+})
+
+
+app.get('/', (request, response) => {
+   response.render('home.hbs', {
+       pageTitle: 'Home Page',
+       where: 'home'
+   })
+});
+
+app.get('/about', (request, response) => {
+   response.render('about.hbs', {
+       pageTitle: 'About Page'
+   }) 
+});
+
+app.listen(3000, () => {
+    console.log('server is up on port 3000');
+});
+
